f2c7bf52ea2d43d33c8ccd273df0b995235c373b
npiskunova <nadejda.piskunova123@gmail.com>
2022-08-16 00:28:04 +0300

    added SelenideWiki and SelenideRepository

---
+++ src/test/java/SelenideWiki.java
@@ -0,0 +1,31 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideWiki {
+    @BeforeAll
+    static void configure() {
+        Configuration.baseUrl = "https://github.com";
+        Configuration.browser = "chrome";
+    }
+    @Test
+    void WikiTest(){
+        //открыть страницу selenide в github
+        open("/selenide/selenide");
+        //перейти в раздел wiki
+        $("#wiki-tab").click();
+        //показать все страницы в списке
+        $("#wiki-pages-box li.wiki-more-pages-link button").click();
+        // проверить что есть страница SoftAssertions
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+        //открыть страницу SoftAssertions
+        $("#wiki-body").find(byText("Soft assertions")).click();
+        //проверить что внутри есть пример кода для JUnit5
+        $("#wiki-content").shouldHave(text("Using JUnit5 extend test class"));
+
+    }
+}
---
+++ src/test/java/github/SelenideRepository.java
@@ -0,0 +1,23 @@
+package github;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideRepository {
+       @Test
+    void RepositoryTest() {
+        // открыть страницу github.com
+        open("https://github.com/");
+        // ввести в поле поиска selenide и нажать Enter
+        $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
+
+        // нажимаем на линк от первого результата поиска
+        $$("ul.repo-list li").first().$("a").click();
+        // check: в заголовке встречается selenide/selenide
+        $("#repository-container-header").shouldHave(text("selenide / selenide"));
+    }
+}
